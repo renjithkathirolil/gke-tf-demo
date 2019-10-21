6fdd2aa4af685d4c9e247b8023b10c8aacc7499f
Renjith Kathirolil <renjithkathirolil@gmail.com>
2019-10-21 08:22:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ resource "aws_ecs_service" "mongo" {
   cluster         = "${aws_ecs_cluster.foo.id}"
   task_definition = "${aws_ecs_task_definition.mongo.arn}"
   desired_count   = 3
-  iam_role        = "${aws_iam_role.foo.arn}"
+  
   depends_on      = ["aws_iam_role_policy.foo"]
 
   ordered_placement_strategy {
